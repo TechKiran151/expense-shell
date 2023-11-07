1cd39ddc9decac6e6ebc06c228fe298862b1ffcd
Kiran Y M <abhikiran171@gmail.com>
2023-11-07 17:52:17 +0530

    made changes

--- frontend.sh
+++ frontend.sh
@@ -26,19 +26,19 @@ color="\e[36m"
 echo -e "${color} Installing Nginx \e[0m"
 dnf install nginx -y &>>$log_file
 
-echo -e "${color}  Copy the expense config file \e[0m"
+echo -e "${color} Copy the expense config file \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 
-echo -e "${color}  Clean/remove old Nginx content \e[0m"
+echo -e "${color} Clean/remove old Nginx content \e[0m"
 rm -rf /usr/share/nginx/html/* &>>$log_file
 
-echo -e "${color}  download the frontend application code \e[0m"
+echo -e "${color} download the frontend application code \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
 
-echo -e "${color}  Extract downloaded application content \e[0m"
+echo -e "${color} Extract downloaded application content \e[0m"
 cd /usr/share/nginx/html &>>$log_file
 unzip /tmp/frontend.zip &>>$log_file
 
-echo -e "${color}  Starting Nginx Service \e[0m"
+echo -e "${color} Starting Nginx Service \e[0m"
 systemctl enable nginx &>>$log_file
 systemctl restart nginx &>>$log_file
