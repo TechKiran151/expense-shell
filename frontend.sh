360522887ad7cb41bc66c4af35294574d5492403
Kiran Y M <abhikiran171@gmail.com>
2023-11-07 14:57:21 +0530

    expense project

--- frontend.sh
+++ frontend.sh
@@ -4,13 +4,13 @@ dnf install nginx -y
 echo -e "\[36m Copy the expense config file \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf
 
-echo -e "\e[36m Clean/remove old Nginx content \[0m"
+echo -e "\e[36m Clean/remove old Nginx content \e[0m"
 rm -rf /usr/share/nginx/html/*
 
 echo -e "\e[36m download the frontend application code \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
-echo -e "\e[36m Extract downloaded application content \[0m"
+echo -e "\e[36m Extract downloaded application content \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
