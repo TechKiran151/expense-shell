20783d32f283a944bc516240aa00f29f82257ee4
Kiran Y M <abhikiran171@gmail.com>
2023-11-07 11:19:28 +0530

    Automated expense project

---
+++ backend.sh
@@ -0,0 +1,23 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+
+dnf install nodejs -y
+
+useradd expense
+
+mkdir /app
+
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+cd /app
+unzip /tmp/backend.zip
+
+npm install
+
+cp backend.service /etc/systemd/system/backend.service
+
+systemctl enable backend
+systemctl start backend
+
+dnf install mysql -y
+
+mysql -h mysql-dev.kiran85.online -uroot -pExpenseApp@1 /app/schema/backend.sql
